3f90b90aece12c643f1c08897268c816c2ec6ee3
Colin <colinjoe9@gmail.com>
2021-07-10 18:35:16 -0400

    fixed build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-"C:/Program Files/Python37/python.exe" -m PyInstaller main.py --name="valorant-rpc" --icon=favicon.ico --onefile
+"C:/Program Files/Python37/python.exe" -m PyInstaller main.py --name="valorant-rpc" --icon=favicon.ico --onefile --hidden-import "pystray._win32
 pause
