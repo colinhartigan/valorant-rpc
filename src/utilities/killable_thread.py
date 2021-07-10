2e1adb3427c5c7450325b2feae2554784296b60e
Colin <colinjoe9@gmail.com>
2021-07-10 18:35:49 -0400

    new thread thing

---
+++ src/utilities/killable_thread.py
@@ -0,0 +1,15 @@
+import threading
+
+class Thread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self,  *args, **kwargs):
+        super(Thread, self).__init__(*args, **kwargs)
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
