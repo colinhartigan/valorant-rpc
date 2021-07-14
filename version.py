0ce663b8d1fa0c5b052127e0467a8a66cdd4410d
Ozama <72144299+OzamaDz@users.noreply.github.com>
2021-07-14 16:31:44 +0100

    Create version.py

---
+++ version.py
@@ -0,0 +1,22 @@
+VSVersionInfo(
+    ffi=FixedFileInfo(
+  filevers=(1, 1, 0, 30920),
+  prodvers=(1, 1, 0, 30920),
+  mask=0x3f,
+  flags=0x0,
+  OS=0x4,
+  fileType=0x1,
+  subtype=0x0,
+  date=(0, 0)),
+    kids=[
+  StringFileInfo([StringTable(
+      u'040904B0',
+      [StringStruct(u'CompanyName', u'colin'),
+      StringStruct(u'FileDescription', u'VALORANT-RPC'),
+      StringStruct(u'FileVersion', u'1.1'),
+      StringStruct(u'InternalName', u'VALORANT-RPC'),
+      StringStruct(u'LegalCopyright', u'colin'),
+      StringStruct(u'OriginalFilename', u'VALORANT-RPC'),
+      StringStruct(u'ProductName', u'RPC'),
+      StringStruct(u'ProductVersion', u'v3')])]), 
+  VarFileInfo([VarStruct(u'Translation', [1033, 1200])])])
