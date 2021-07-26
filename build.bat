f768a7e6a7949aa989d2d6a532fd08aab1e79546
Colin <colinjoe9@gmail.com>
2021-07-26 02:22:03 -0400

    rc2

--- build.bat
+++ build.bat
@@ -28,6 +28,7 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------    
+	"C:/Program Files/Python39/python.exe" -m pipreqs.pipreqs --force --no-pin
     "C:/Program Files/Python37/python.exe" -m pip install -r requirements.txt
     "C:/Program Files/Python37/python.exe" -m pip install -r requirements.txt --upgrade
     "C:/Program Files/Python37/python.exe" -m PyInstaller main.py --name="valorant-rpc" --icon=favicon.ico --hidden-import "pystray._win32" --onefile --version-file "version.py"
