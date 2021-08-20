7ef60949a4918e9b9516deb7f61792d6b9314642
victor <vjoao194@gmail.com>
2021-08-19 21:22:48 -0300

    fixes mode and level not localized in queue
    not sure if that's exactly how you want it, but it works

--- src/presence/presences/menu_presences/queue.py
+++ src/presence/presences/menu_presences/queue.py
@@ -5,14 +5,14 @@ def presence(rpc,client=None,data=None,content_data=None,config=None):
     
     party_state,party_size = Utilities.build_party_state(data)
     start_time = Utilities.iso8601_to_epoch(data['queueEntryTime'])
-
+    small_image, mode_name = Utilities.fetch_mode_data(data, content_data)
     rpc.update(
         state=party_state,
-        details=f"{Localizer.get_localized_text('presences','client_states','queue')} - {content_data['queue_aliases'][data['queueId']]}",
+        details=f"{Localizer.get_localized_text('presences','client_states','queue')} - {mode_name}",
         start=start_time,
         large_image="game_icon_white",
-        large_text=f"Level {data['accountLevel']}",
-        small_image=f"mode_{data['queueId'] if data['queueId'] in content_data['modes_with_icons'] else 'unrated'}",
+        large_text=f"{Localizer.get_localized_text('presences','leveling','level')} {data['accountLevel']}",
+        small_image=small_image,
         party_size=party_size,
         party_id=data["partyId"],
     )
