5b1506b9ffc840726764f9ffe50b6e13dbc5f2f5
victor <vjoao194@gmail.com>
2021-08-21 20:18:18 -0300

    away now formatted consistent to other states
    again again cause typo

--- src/presence/presences/menu_presences/away.py
+++ src/presence/presences/menu_presences/away.py
@@ -4,7 +4,7 @@ from ...presence_utilities import Utilities
 def presence(rpc,client=None,data=None,content_data=None,config=None):
     _, mode_name = Utilities.fetch_mode_data(data,content_data)
     rpc.update(
-        detail=f"{mode_name}",
+        details=f"{mode_name}",
         state=f"{Localizer.get_localized_text('presences','client_states','away')}",
         large_image="game_icon_yellow",
         large_text="VALORANT",
