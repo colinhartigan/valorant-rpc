a2b61fa4f018bd15bf9ef0548b20e1a031c328d1
janinha <vjoao194@gmail.com>
2021-10-31 19:12:37 -0300

    Update default.py
    crazy way to fix crashing when you don't want to show ranks in lobby

--- src/presence/presences/menu_presences/default.py
+++ src/presence/presences/menu_presences/default.py
@@ -11,13 +11,25 @@ def presence(rpc,client=None,data=None,content_data=None,config=None):
     else:
         party_state,party_size = Utilities.build_party_state(data)
         small_image, mode_name = Utilities.fetch_mode_data(data,content_data)
-        small_text = None
+        small_text = mode_name
         buttons = Utilities.get_join_state(client,config,data)
 
         if data["queueId"] == "competitive" and Localizer.get_config_value("presences","menu","show_rank_in_comp_lobby"): 
             small_image, small_text = Utilities.fetch_rank_data(client,content_data)
 
-        rpc.update(
+            rpc.update(
+            state=party_state,
+            details=f"{Localizer.get_localized_text('presences','client_states','menu')} - {mode_name}",
+            large_image="game_icon",
+            large_text=f"{Localizer.get_localized_text('presences','leveling','level')} {data['accountLevel']}",
+            small_image=small_image,
+            small_text=small_text,
+            party_size=party_size,
+            party_id=data["partyId"],
+            buttons=buttons
+        )
+        else:
+            rpc.update(
             state=party_state,
             details=f"{Localizer.get_localized_text('presences','client_states','menu')} - {mode_name}",
             large_image="game_icon",
