9defa650145f748b6b3a72ad99b6730c9d258d3a
janinha <vjoao194@gmail.com>
2021-10-31 19:12:15 -0300

    Update away.py
    crazy way to fix crashing when you don't want to show ranks in lobby

--- src/presence/presences/menu_presences/away.py
+++ src/presence/presences/menu_presences/away.py
@@ -4,11 +4,22 @@ from ...presence_utilities import Utilities
 def presence(rpc,client=None,data=None,content_data=None,config=None):
     _, mode_name = Utilities.fetch_mode_data(data,content_data)
     
+    small_image, mode_name = Utilities.fetch_mode_data(data,content_data)
+    small_text = mode_name
     party_state,party_size = Utilities.build_party_state(data)
     if data["queueId"] == "competitive" and Localizer.get_config_value("presences","menu","show_rank_in_comp_lobby"): 
         small_image, small_text = Utilities.fetch_rank_data(client,content_data)
-    
-    rpc.update(
+        
+        rpc.update(
+        details=f"{Localizer.get_localized_text('presences','client_states','menu')} - {mode_name}",
+        state=f"{Localizer.get_localized_text('presences','client_states','away')} (" + party_state + ")",
+        large_image="game_icon_yellow",
+        large_text=f"{Localizer.get_localized_text('presences','leveling','level')} {data['accountLevel']}",
+        small_image=small_image,
+        small_text=small_text,
+    )
+    else:
+        rpc.update(
         details=f"{Localizer.get_localized_text('presences','client_states','menu')} - {mode_name}",
         state=f"{Localizer.get_localized_text('presences','client_states','away')} (" + party_state + ")",
         large_image="game_icon_yellow",
