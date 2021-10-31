716cedb4114f88eab917b4caaa60517a2daea7fb
janinha <vjoao194@gmail.com>
2021-10-31 18:32:22 -0300

    Update startup.py
    white icon when starting up, gives more of a "not ready" vibe, if you know what I mean

--- src/presence/presences/startup.py
+++ src/presence/presences/startup.py
@@ -3,7 +3,7 @@ from ...localization.localization import Localizer
 def presence(rpc,client=None,data=None,content_data=None,config=None):
     rpc.update(
         state=Localizer.get_localized_text("presences","startup","loading"),
-        large_image="game_icon",
+        large_image="game_icon_white",
         large_text="VALORANT-rpc",
         buttons=[{
             'label':Localizer.get_localized_text("presences","startup","view_github"),
