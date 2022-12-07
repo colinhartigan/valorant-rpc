cc9396aaaa18ac554230ac11c1fad66095d019b9
Colin <colinjoe9@gmail.com>
2022-12-06 22:38:07 -0500

    new

--- src/utilities/config/app_config.py
+++ src/utilities/config/app_config.py
@@ -7,7 +7,7 @@ from ...localization.locales import Locales
 from ...localization.localization import Localizer
 
 default_config = {
-    "version": "v3.2.1",
+    "version": "v3.2.2",
     "region": ["",Client.fetch_regions()],
     "client_id": 811469787657928704,
     "presence_refresh_interval": 3,
